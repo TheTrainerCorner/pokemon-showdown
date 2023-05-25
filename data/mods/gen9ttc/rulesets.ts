71128f5d4def8975d365460aaa21050050cf5578
Cody Spratford <koreanpanda345@gmail.com>
2023-05-25 16:58:53 -0700

    Update rulesets.ts

--- data/mods/gen9ttc/rulesets.ts
+++ data/mods/gen9ttc/rulesets.ts
@@ -4,9 +4,9 @@ export const Rulesets: {[k: string]: ModdedFormatData} = {
 		name: "TTC Draft",
 		desc: "The custom draft league ruleset",
 		ruleset: [
-			'Obtainable', 'Team Preview', 'Sleep Clause Mod', 'OHKO Clause', 'Evasion Clause', 'Endless Battle Clause', 'HP Percentage Mod', 'Cancel Mod'
+			'Standard NatDex',
 		],
 		banlist: ['Banned'],
-		
+
 	}
 }
