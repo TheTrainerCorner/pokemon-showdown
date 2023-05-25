b8a2ba776073cb0e50fb339ea0b037f93ee99a62
Cody Spratford <koreanpanda345@gmail.com>
2023-05-25 16:56:07 -0700

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -10,7 +10,7 @@ export const Formats: FormatList = [
 		mod: 'gen9ttc',
 		desc: "The Offical Draft format for TTC! Tiers in this format are different and refelect the league's point tiers. If you are here from a different league, then It is recommended that you use [Gen 9] Other Draft.",
 		ruleset: [
-			'TTC Draft', 'Obtainable', '+Unreleased', '+CAP', 'Team Preview', 'Sleep Clause Mod', 'OHKO Clause', 'Evasion Clause', 'Endless Battle Clause', 'HP Percentage Mod', 'Cancel Mod'
+			'TTC Draft'
 		]
 	}
 ];
