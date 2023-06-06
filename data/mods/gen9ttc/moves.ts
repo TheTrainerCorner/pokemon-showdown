de3186f971713f2f1b2702eb1b55429a53482545
Cody Spratford <koreanpanda345@gmail.com>
2023-06-06 13:41:10 -0700

    Update moves.ts

--- data/mods/gen9ttc/moves.ts
+++ data/mods/gen9ttc/moves.ts
@@ -2,5 +2,21 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 	shedtail: {
 		inherit: true,
 		pp: 1,
-	}
+	},
+	present: {
+		inherit: true,
+		onModifyMove(move, pokemon, target) {
+			const rand = this.random(10);
+			if(rand < 2) {
+				move.heal = [1, 4];
+				move.infiltrates = true;
+			} else if (rand < 6) {
+				move.basePower = 50;
+			} else if (rand < 9) {
+				move.basePower = 100;
+			} else {
+				move.basePower = 150;
+			}
+		}
+	},
 };
