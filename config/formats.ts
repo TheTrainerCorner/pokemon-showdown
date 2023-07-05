09b88fb93ca1ca87a95bbf37a3484d000e70d3df
Cody Spratford <koreanpanda345@gmail.com>
2023-07-04 17:45:43 -0700

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -23,23 +23,19 @@ export const Formats: FormatList = [
 		section: "Draft",
 	},
 	{
-		name: "[Gen 9] TTC Draft",
+		name: "[Gen 9] National Dex Draft",
 		desc: "The Official Draft Format for TTC",
 		mod: 'gen9ttc',
-		ruleset: [
-			'Obtainable',
-			'+Past', 
-			'Species Clause',
-			'OHKO Clause',
-			'Evasion Items Clause',
-			'Evasion Moves Clause',
-			'Evasion Abilities Clause',
-			'Moody Clause',
-			'Swagger Clause',
-			'Team Preview',
-			'Nickname Clause',
-			'+Unobtainable',
-		]
+		ruleset: ['Standard NatDex', 'OHKO Clause', 'Evasion Clause', 'Species Clause', 'Sleep Clause Mod'],
+		banlist: [
+			'ND Uber', 'ND AG', 'Arena Trap', 'Moody', 'Power Construct', 'Shadow Tag', 'King\'s Rock',
+			'Quick Claw', 'Razor Fang', 'Assist', 'Baton Pass', 'Last Respects', 'Shed Tail',
+		],
+		unbanlist: [
+			'Springtide Storm',
+			'Zippy Zap',
+			'Sappy Seed'
+		],
 	},
 	{
 		section: "Public Test Realm",
@@ -47,19 +43,16 @@ export const Formats: FormatList = [
 	{
 		name: "[Gen 9] TTC PTR",
 		desc: "This is the Official PTR for TTC.",
-		ruleset: [
-			'Obtainable',
-			'+Past', 
-			'Species Clause',
-			'OHKO Clause',
-			'Evasion Items Clause',
-			'Evasion Moves Clause',
-			'Evasion Abilities Clause',
-			'Moody Clause',
-			'Swagger Clause',
-			'Team Preview',
-			'Nickname Clause',
-			'+Unobtainable',
+		mod: 'gen9ttc',
+		ruleset: ['Standard NatDex', 'OHKO Clause', 'Evasion Clause', 'Species Clause', 'Sleep Clause Mod'],
+		banlist: [
+			'ND Uber', 'ND AG', 'Arena Trap', 'Moody', 'Power Construct', 'Shadow Tag', 'King\'s Rock',
+			'Quick Claw', 'Razor Fang', 'Assist', 'Baton Pass', 'Last Respects', 'Shed Tail',
+		],
+		unbanlist: [
+			'Springtide Storm',
+			'Zippy Zap',
+			'Sappy Seed'
 		],
 	}
 
