ce6b48612d8d65dd3cf24aa0863ed39f9bffb7e3
Cody Spratford <koreanpanda345@gmail.com>
2023-07-04 17:15:27 -0700

    Fixing things

--- data/mods/gen9ttc/format-data.ts
+++ data/mods/gen9ttc/format-data.ts
@@ -1,17 +1,2 @@
 export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
-	venusaur: {
-		draftTier: '9'
-	},
-	charizard: {
-		draftTier: '8',
-	},
-	charizardmegax: {
-		draftTier: '17',
-	},
-	charizardmegay: {
-		draftTier: '14'
-	},
-	blastoise: {
-		draftTier: '10'
-	}
 };
--- data/mods/gen9ttc/rulesets.ts
+++ data/mods/gen9ttc/rulesets.ts
@@ -1,51 +1,12 @@
 export const Rulesets: {[k: string]: ModdedFormatData} = {
-	standard: {
-		inherit: true,
-		ruleset: ['Standard NatDex'],
-		unbanlist: [
-			// Unobtainable
-			'Barb Barrage',
-			'Blazing Torque',
-			// 'Bleakwind Storm',
-			'Ceaseless Edge',
-			'Chloroblast',
-			'Combat Torque',
-			'Cosmic Power',
-			'Dire Claw',
-			'Esper Wing',
-			'Heal Bell',
-			'Infernal Parade',
-			// 'Lunar Blessing',
-			'Magical Torque',
-			'Mountain Gale',
-			// 'Mystical Power',
-			'Noxious Torque',
-			'Psyshield Bash',
-			'Raging Fury',
-			// 'Sandsear Storm',
-			'Shelter',
-			'Simple Beam',
-			'Springtide Storm',
-			'Stone Axe',
-			'Triple Axel',
-			'Victory Dance',
-			'Wicked Torque',
-			// 'Wildbolt Storm',
+	ttcdraft: {
+		effectType: "ValidatorRule",
+		name: "TTC Draft",
+		desc: "The custom draft league ruleset",
+		ruleset: [
+			'Standard NatDex',
+		],
+		banlist: [],
 
-			// LGPE
-			"Baddy Bad",
-			"Bouncy Bubble",
-			"Buzzy Buzz",
-			"Floaty Fall",
-			"Freezy Frost",
-			"Glitzy Glow",
-			"Pikapapow",
-			"Sappy Seed",
-			"Sizzly Slide",
-			"Sparkly Swirl",
-			"Splishy Splash",
-			"Veeveevolley",
-			"Zippy Zap",
-		]
 	}
 }
