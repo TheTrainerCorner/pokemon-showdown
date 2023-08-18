aa7943fc01a7a29264a94c486e4fe9a6c7d09559
Cody Spratford <koreanpanda345@gmail.com>
2023-08-18 16:12:23 -0700

    Update rulesets.ts

--- data/mods/gen9ttc/rulesets.ts
+++ data/mods/gen9ttc/rulesets.ts
@@ -4,7 +4,7 @@ export const Rulesets: {[k: string]: ModdedFormatData} = {
 		name: 'TTC Single',
 		desc: "The Trainer's Corner Single Battle ruleset.",
 		ruleset: [
-			'Standard', 'Dynamax Clause', 'Gems Clause',
+			'Standard', 'Dynamax Clause', 'Gems Clause', '+Past', '+Unobtainable'
 		],
 		banlist: [
 			'Uber', 'AG',
