a753eb0ecf2130a2f4d0dfe651d5c68e4f48cc13
Cody Spratford <koreanpanda345@gmail.com>
2023-08-19 16:15:17 -0700

    adding a clause to prevent Shell smash on Mega Toise

--- data/mods/gen9ttc/rulesets.ts
+++ data/mods/gen9ttc/rulesets.ts
@@ -1,3 +1,21 @@
 export const Rulesets: {[k: string]: ModdedFormatData} = {
-
+	noshellsmashmegatoise: {
+		effectType: 'ValidatorRule',
+		name: 'No Shell Smash Mega Toise',
+		desc: "Prevents Mega Blastoise from having Shell Smash",
+		onValidateSet(set) {
+			const problems = [];
+			let toise = this.dex.species.get('Blastoise');
+			let megaStone = this.dex.items.get('Blastoisinite');
+			let shellSmash = this.dex.moves.get('Shell Smash');
+			if(set.species === toise.name) {
+				if(set.item === megaStone.name) {
+					if(set.moves.includes(shellSmash.name)) {
+						problems.push(`Shell Smash is banned on Mega Blastoise.`);
+					}
+				}
+			}
+			return problems;
+		}
+	}
 }
