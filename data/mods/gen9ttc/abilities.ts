2c8af366ca1aa92657a90db909ab2411dbe00f9b
Cody Spratford <koreanpanda345@gmail.com>
2023-08-21 11:35:22 -0700

    Fixing Grass Pelt
    Changed the priority of SpD on grass pelt. it should trigger after the increase on Def.

--- data/mods/gen9ttc/abilities.ts
+++ data/mods/gen9ttc/abilities.ts
@@ -293,7 +293,7 @@ export const Abilities: {[k: string]: ModdedAbilityData} = {
 	grasspelt: {
 		inherit: true,
 		onModifyDefPriority: 6,
-		onModifySpDPriority: 6,
+		onModifySpDPriority: 7,
 		onModifyDef(pokemon) {
 			if (this.field.isTerrain('grassyterrain')) return this.chainModify(1.5);
 			else return this.chainModify(1.25);
