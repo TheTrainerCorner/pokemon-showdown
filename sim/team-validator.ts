d6201068e8fe3b18d64b2f79216e018ca961bfc4
Cody Spratford <koreanpanda345@gmail.com>
2023-08-20 17:30:38 -0700

    Update team-validator.ts

--- sim/team-validator.ts
+++ sim/team-validator.ts
@@ -589,7 +589,7 @@ export class TeamValidator {
 				if (!ability.name || ability.name === 'No Ability') {
 					problems.push(`${name} needs to have an ability.`);
 				} else if (!Object.values(species.abilities).includes(ability.name)) {
-					console.log(tierSpecies.abilities);
+					console.log(tierSpecies);
 					if (tierSpecies.abilities[0] === ability.name) {
 						set.ability = species.abilities[0];
 					} else {
