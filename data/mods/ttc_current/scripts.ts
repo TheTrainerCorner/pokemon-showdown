66a233f64ed5c5a1d19acc4bca776ff9c46d8639
Cody Spratford <koreanpanda345@gmail.com>
2023-09-13 14:25:37 -0700

    Just added a quick comment to explain the intention of the folder.

--- data/mods/ttc_current/scripts.ts
+++ data/mods/ttc_current/scripts.ts
@@ -1,6 +1,6 @@
 export const Scripts: ModdedBattleScriptsData = {
 	inherit: 'ttc1_1',
 	init() {
-		
+		// This folder shouldn't be touched. It is only used to direct the pointers to the correct place.
 	}
 }
