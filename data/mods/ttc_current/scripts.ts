79708e9483027bc1c05cff4df3e980e3b8a53e49
Cody Spratford <koreanpanda345@gmail.com>
2023-09-15 12:49:45 -0700

    adding the tiers to the current folder

--- data/mods/ttc_current/scripts.ts
+++ data/mods/ttc_current/scripts.ts
@@ -2,5 +2,8 @@ export const Scripts: ModdedBattleScriptsData = {
 	inherit: 'ttc1_1',
 	init() {
 		// This folder shouldn't be touched. It is only used to direct the pointers to the correct place.
+		this.modData('FormatsData', 'kingambit').natDexTier = 'OU';
+		this.modData('FormatsData', 'blastoisemega').natDexTier = 'OU';
+		this.modData('FormatsData', 'ursaluna').natDexTier = 'OU';
 	}
 }
