f262efbaec35ba6ada05ccc527bad84dfbdaf0c3
Cody Spratford <koreanpanda345@gmail.com>
2023-09-23 14:12:29 -0700

    Adding in Slow Start

--- data/mods/ttc1_1/abilities.ts
+++ data/mods/ttc1_1/abilities.ts
@@ -1,5 +1,11 @@
-import {Ability} from '../../../sim/dex-abilities';
-
 export const Abilities: { [k: string]: ModdedAbilityData} = {
-
+	slowstart: {
+		inherit: true,
+		condition: {
+			onModifyAtkPriority: undefined,
+			onModifyAtk: undefined,
+		},
+		desc: "On switch-in, this Pokemon's Speed are halved for 5 Turns.",
+		shortDesc: "On switch-in, this Pokemon's Speed are halved for 5 Turns.",
+	}
 };
