1eaa9e27206274c636e42c6559e2cc0786594bca
Cody Spratford <koreanpanda345@gmail.com>
2023-10-03 12:15:20 -0700

    Fixed the issue of Slow Start not activating
    Me was being dumb, but because it is object based, I forgot that I need to rebuild the condition prop.

--- data/mods/ttc1_1/abilities.ts
+++ data/mods/ttc1_1/abilities.ts
@@ -2,8 +2,18 @@ export const Abilities: { [k: string]: ModdedAbilityData} = {
 	slowstart: {
 		inherit: true,
 		condition: {
-			onModifyAtkPriority: undefined,
-			onModifyAtk: undefined,
+			duration: 5,
+			onResidualOrder: 28,
+			onResidualSubOrder: 2,
+			onStart(target) {
+				this.add('-start', target, 'ability: Slow Start');
+			},
+			onModifySpe(spe, pokemon) {
+				return this.chainModify(0.5);
+			},
+			onEnd(target) {
+				this.add('-end', target, 'Slow Start')
+			},
 		},
 		desc: "On switch-in, this Pokemon's Speed are halved for 5 Turns.",
 		shortDesc: "On switch-in, this Pokemon's Speed are halved for 5 Turns.",
