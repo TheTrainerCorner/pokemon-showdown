dc434fc19f4888ab871c4b5c6a67d1f0f670cd82
Cody Spratford <koreanpanda345@gmail.com>
2023-10-08 18:46:53 -0700

    Modifying RKS System
    Making RKS system to modify Silvally's type based on the first move used.

--- data/mods/ttc1_1/abilities.ts
+++ data/mods/ttc1_1/abilities.ts
@@ -17,5 +17,23 @@ export const Abilities: { [k: string]: ModdedAbilityData} = {
 		},
 		desc: "On switch-in, this Pokemon's Speed are halved for 5 Turns.",
 		shortDesc: "On switch-in, this Pokemon's Speed are halved for 5 Turns.",
-	}
+	},
+	rkssystem: {
+		inherit: true,
+		onPrepareHit(source, target, move) {
+			if(source.baseSpecies.name !== 'Silvally') return;
+			if(this.effectState.rkssystem) return;
+			if(move.hasBounced || move.flags['futuremove'] || move.sourceEffect !== 'snatch') return;
+			const type = move.type;
+			if(type && type !== '???' && source.getTypes().join() !== type) {
+				if(!source.formeChange(`Silvally-${type}`, this.effect, true)) return;
+				this.effectState.rkssystem = true;
+			}
+		},
+		onSwitchIn(pokemon) {
+			delete this.effectState.rkssystem;
+		},
+		desc: "If this Pokemon is Silvally; Silvally will change its form to match the type of the move it is about to use. This effect comes after all effects that change a move's type. This effect can only happen once per switch-in, and only if this Pokemon is not Terastallized.",
+		shortDesc: "If Silvally; Changes Form based on the type of the move about to use.",
+	},
 };
---
+++ data/mods/ttc1_1/conditions.ts
@@ -0,0 +1,7 @@
+export const Conditions: {[k: string]: ModdedConditionData} = {
+	silvally: {
+		inherit: true,
+		onType: undefined,
+		onTypePriority: undefined,
+	},
+};
