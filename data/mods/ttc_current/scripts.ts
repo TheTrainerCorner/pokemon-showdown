5b5ad87bf9e631e90abb817adb15a933347d1fc8
Cody Spratford <koreanpanda345@gmail.com>
2023-10-20 17:26:34 -0700

    Reverting

--- data/mods/ttc_current/scripts.ts
+++ data/mods/ttc_current/scripts.ts
@@ -1,5 +1,5 @@
 export const Scripts: ModdedBattleScriptsData = {
-	inherit: 'ttc1_2',
+	inherit: 'ttc1_1',
 	init() {
 		// This folder shouldn't be touched. It is only used to direct the pointers to the correct place.
 
