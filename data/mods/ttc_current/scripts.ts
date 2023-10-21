aad4f5f6319998555b509745f751f3cb8712fa69
Cody Spratford <koreanpanda345@gmail.com>
2023-10-20 17:02:31 -0700

    Testing TTC_Halloween_Event

--- data/mods/ttc_current/scripts.ts
+++ data/mods/ttc_current/scripts.ts
@@ -1,9 +1,7 @@
 export const Scripts: ModdedBattleScriptsData = {
-	inherit: 'ttc1_1',
+	inherit: 'ttc1_2',
 	init() {
 		// This folder shouldn't be touched. It is only used to direct the pointers to the correct place.
-		this.modData('FormatsData', 'kingambit').natDexTier = 'OU';
-		this.modData('FormatsData', 'blastoisemega').natDexTier = 'OU';
-		this.modData('FormatsData', 'ursaluna').natDexTier = 'OU';
+
 	},
 };
