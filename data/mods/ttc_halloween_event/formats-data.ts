405dfa30e59e9584cfed5624b7f67d56259a0928
Cody Spratford <koreanpanda345@gmail.com>
2023-10-30 11:18:59 -0700

    Adding the halloween pokemon to the tiers.

---
+++ data/mods/ttc_halloween_event/formats-data.ts
@@ -0,0 +1,22 @@
+export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
+	bulbick: {
+		isNonstandard: "Past",
+		tier: "LC",
+		natDexTier: "LC",
+	},
+	lampoct: {
+		isNonstandard: "Past",
+		tier: "NFE",
+		natDexTier: "NFE",
+	},
+	octolure: {
+		isNonstandard: "Past",
+		tier: "OU",
+		natDexTier: "OU",
+	},
+	gourgeistmega: {
+		isNonstandard: "Past",
+		tier: "OU",
+		natDexTier: "OU",
+	},
+};
