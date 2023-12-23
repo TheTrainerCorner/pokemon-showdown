4e106447a2bdbbce5c345606b230b235f83f8754
Cody Spratford <koreanpanda345@gmail.com>
2023-12-22 16:56:08 -0800

    Create formats-data.ts

---
+++ data/mods/ttc_season_1_donations/formats-data.ts
@@ -0,0 +1,32 @@
+export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
+	sotanaht: {
+		isNonstandard: "Past",
+		tier: "OU",
+		natDexTier: "OU",
+	},
+	tindraco: {
+		isNonstandard: "Past",
+		tier: "LC",
+		natDexTier: "LC",
+	},
+	devdraco: {
+		isNonstandard: "Past",
+		tier: 'NFE',
+		natDexTier: "NFE",
+	},
+	metadraco: {
+		isNonstandard: "Past",
+		tier: "OU",
+		natDexTier: "OU",
+	},
+	acudraco: {
+		isNonstandard: "Past",
+		tier: "OU",
+		natDexTier: "OU",
+	},
+	thundraco: {
+		isNonstandard: "Past",
+		tier: "OU",
+		natDexTier: "OU",
+	},
+}
