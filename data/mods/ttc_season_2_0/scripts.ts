9f4acfd755814a64022dba434075baeb74c4811f
Cody Spratford <koreanpanda345@gmail.com>
2023-12-29 20:43:27 -0800

    Update scripts.ts

--- data/mods/ttc_season_2_0/scripts.ts
+++ data/mods/ttc_season_2_0/scripts.ts
@@ -791,7 +791,7 @@ export const Scripts: ModdedBattleScriptsData = {
 					.setSPA(45)
 					.setSPD(90)
 					.setSPE(75);
-			new ModifyPokemon('Trangquil', this)
+			new ModifyPokemon('Tranquil', this)
 				.types
 					.setType('Flying')
 				.pokemon.abilities
