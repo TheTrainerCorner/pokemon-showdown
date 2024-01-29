c966628b1b8d795668ae200e9d2ba839c3901aa9
Cody Spratford <koreanpanda345@gmail.com>
2024-01-29 11:47:51 -0800

    Create formats-data.ts

---
+++ data/mods/ttc_season_2_0/formats-data.ts
@@ -0,0 +1,87 @@
+export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
+	inteleonmega: {
+		isNonstandard: "Past",
+		tier: 'OU',
+		natDexTier: "OU",
+	},
+	granbullmega: {
+		isNonstandard: "Past",
+		tier: 'OU',
+		natDexTier: 'OU',
+	},
+	sotanaht: {
+		isNonstandard: "Past",
+		tier: "OU",
+		natDexTier: "OU",
+	},
+	tindraco: {
+		isNonstandard: "Past",
+		tier: "LC",
+		natDexTier: "LC",
+	},
+	devdraco: {
+		isNonstandard: "Past",
+		tier: 'NFE',
+		natDexTier: "NFE",
+	},
+	metadraco: {
+		isNonstandard: "Past",
+		tier: "OU",
+		natDexTier: "OU",
+	},
+	acudraco: {
+		isNonstandard: "Past",
+		tier: "OU",
+		natDexTier: "OU",
+	},
+	thundraco: {
+		isNonstandard: "Past",
+		tier: "OU",
+		natDexTier: "OU",
+	},
+	farfetchdmega: {
+		isNonstandard: "Past",
+		tier: "OU",
+		natDexTier: "OU",
+	},
+	poisonus: {
+		isNonstandard: "Past",
+		tier: "OU",
+		natDexTier: "OU",
+	},
+	poisonustherian: {
+		isNonstandard: "Past",
+		tier: "OU",
+		natDexTier: "OU",
+	},
+	volcaronafaerie: {
+		isNonstandard: "Past",
+		tier: "OU",
+		natDexTier: "OU",
+	},
+	raltsolympios: {
+		isNonstandard: "Past",
+		tier: "LC",
+		natDexTier: "LC",
+	},
+	kirliaolympios: {
+		isNonstandard: "Past",
+		tier: "NFE",
+		natDexTier: "NFE",
+	},
+	gardevoirolympios: {
+		isNonstandard: "Past",
+		tier: "OU",
+		natDexTier: "OU",
+	},
+	galladeolympios: {
+		isNonstandard: "Past",
+		tier: "OU",
+		natDexTier: "OU",
+	},
+	gigachelonian: {
+		isNonstandard: "Past",
+		tier: "OU",
+		natDexTier: "OU",
+	},
+}
