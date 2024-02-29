75ad082ae5620da7dea9c0869c4788bc07685e1e
Cody Spratford <koreanpanda345@gmail.com>
2024-02-28 16:35:13 -0800

    Update pokedex.ts

--- data/mods/ttc_season_1/pokedex.ts
+++ data/mods/ttc_season_1/pokedex.ts
@@ -1,74 +1,4 @@
 export const Pokedex: {[k: string]: ModdedSpeciesData} = {
-	//#region Version 1.1
-	silvallybug: {
-		inherit: true,
-		requiredItem: undefined
-	},
-	silvallydark: {
-		inherit: true,
-		requiredItem: undefined,
-	},
-	silvallydragon: {
-		inherit: true,
-		requiredItem: undefined,
-	},
-	silvallyelectric: {
-		inherit: true,
-		requiredItem: undefined,
-	},
-	silvallyfairy: {
-		inherit: true,
-		requiredItem: undefined,
-	},
-	silvallyfighting: {
-		inherit: true,
-		requiredItem: undefined,
-	},
-	silvallyfire: {
-		inherit: true,
-		requiredItem: undefined,
-	},
-	silvallyflying: {
-		inherit: true,
-		requiredItem: undefined,
-	},
-	silvallyghost: {
-		inherit: true,
-		requiredItem: undefined,
-	},
-	silvallygrass: {
-		inherit: true,
-		requiredItem: undefined,
-	},
-	silvallyground: {
-		inherit: true,
-		requiredItem: undefined,
-	},
-	silvallyice: {
-		inherit: true,
-		requiredItem: undefined,
-	},
-	silvallypoison: {
-		inherit: true,
-		requiredItem: undefined,
-	},
-	silvallypsychic: {
-		inherit: true,
-		requiredItem: undefined,
-	},
-	silvallyrock: {
-		inherit: true,
-		requiredItem: undefined,
-	},
-	silvallysteel: {
-		inherit: true,
-		requiredItem: undefined,
-	},
-	silvallywater: {
-		inherit: true,
-		requiredItem: undefined,
-	},
-	//#endregion
 	//#region Season 1 Donations
 	sotanaht: {
 		num: -2001,
@@ -479,5 +409,74 @@ export const Pokedex: {[k: string]: ModdedSpeciesData} = {
 		battleOnly: "Meloetta-Caroler",
 	},
 	//#endregion
-	
+	//#region Version 1.1
+	silvallybug: {
+		inherit: true,
+		requiredItem: undefined
+	},
+	silvallydark: {
+		inherit: true,
+		requiredItem: undefined,
+	},
+	silvallydragon: {
+		inherit: true,
+		requiredItem: undefined,
+	},
+	silvallyelectric: {
+		inherit: true,
+		requiredItem: undefined,
+	},
+	silvallyfairy: {
+		inherit: true,
+		requiredItem: undefined,
+	},
+	silvallyfighting: {
+		inherit: true,
+		requiredItem: undefined,
+	},
+	silvallyfire: {
+		inherit: true,
+		requiredItem: undefined,
+	},
+	silvallyflying: {
+		inherit: true,
+		requiredItem: undefined,
+	},
+	silvallyghost: {
+		inherit: true,
+		requiredItem: undefined,
+	},
+	silvallygrass: {
+		inherit: true,
+		requiredItem: undefined,
+	},
+	silvallyground: {
+		inherit: true,
+		requiredItem: undefined,
+	},
+	silvallyice: {
+		inherit: true,
+		requiredItem: undefined,
+	},
+	silvallypoison: {
+		inherit: true,
+		requiredItem: undefined,
+	},
+	silvallypsychic: {
+		inherit: true,
+		requiredItem: undefined,
+	},
+	silvallyrock: {
+		inherit: true,
+		requiredItem: undefined,
+	},
+	silvallysteel: {
+		inherit: true,
+		requiredItem: undefined,
+	},
+	silvallywater: {
+		inherit: true,
+		requiredItem: undefined,
+	},
+	//#endregion
 };
