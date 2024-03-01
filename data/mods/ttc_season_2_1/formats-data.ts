cde73ca58a9f412729df36176632e4eaaf4609f7
Cody Spratford <koreanpanda345@gmail.com>
2024-02-29 19:01:03 -0800

    Update formats-data.ts

--- data/mods/ttc_season_2_1/formats-data.ts
+++ data/mods/ttc_season_2_1/formats-data.ts
@@ -2,5 +2,10 @@ export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
 	baxcalibur: {
 		tier: "OU",
 		natDexTier: "OU",
+	},
+	octolure: {
+		isNonstandard: "Past",
+		tier: "OU",
+		natDexTier: "OU",
 	}
 }
