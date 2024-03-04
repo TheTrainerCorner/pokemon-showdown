4b6a78ae103cb49b975aee7c059e94f6bc4ad54f
Cody Spratford <koreanpanda345@gmail.com>
2024-03-04 14:16:33 -0800

    Update formats-data.ts

--- data/mods/gen9ttc/formats-data.ts
+++ data/mods/gen9ttc/formats-data.ts
@@ -1,6 +1,11 @@
 export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
+	inteleonmega: {
+		natDexTier: "OU",
+		tier: "OU",
+	},
 	trevenantautumn: {
 		isNonstandard: "Past",
+		natDexTier: "OU",
 		tier: "15",
 	},
 };
