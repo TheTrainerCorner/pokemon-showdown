6d1b3e31bdcd060529ce975a849d20081573cc3c
skipprthepenguin <sagie2006p@gmail.com>
2024-03-24 22:45:44 +0200

    Revert "Update abilities.ts"
    This reverts commit 7b0a39da3f0278bda9ec4993e3e7c5ec2ece272b.

--- data/mods/ttc_season_2_0/abilities.ts
+++ data/mods/ttc_season_2_0/abilities.ts
@@ -1290,7 +1290,7 @@ export const Abilities: { [k: string]: ModdedAbilityData} = {
 		},
 		onSourceAfterMove(source, target, move) {
             if (move.category === 'Status') {
-                this.heal(target.baseMaxhp / 8);
+                this.heal(source.baseMaxhp / 8);
             }
         },
 		desc: "The user heals 1/8 of their max hp when using a status move, however that move will have -1 priority.",
