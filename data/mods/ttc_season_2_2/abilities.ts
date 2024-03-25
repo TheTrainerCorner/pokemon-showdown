1f1acede2a8f8d47b79fa3b81afcb1713f739362
Cody Spratford <koreanpanda345@gmail.com>
2024-03-24 21:53:09 -0700

    Made Season 2.2 folder, and adjusted Rivalry

---
+++ data/mods/ttc_season_2_2/abilities.ts
@@ -0,0 +1,14 @@
+export const Abilities: { [k: string]: ModdedAbilityData} = {
+	rivalry: {
+		inherit: true,
+		onModifyDamage(dmg, source, target, move) {
+			if (source === target) return;
+			for (const type of target.types) {
+				if (source.hasType(type)) {
+					// The chain modify was probably not using 1.2 correct, so we will have to be more precise with the math.
+					return this.chainModify([4916, 4096]); // 4916/4096 = ~1.2
+				}
+			}
+		}
+	}
+};
---
+++ data/mods/ttc_season_2_2/scripts.ts
@@ -0,0 +1,6 @@
+export const Scripts: ModdedBattleScriptsData = {
+	inherit: "ttc_season_2_1",
+	init() {
+
+	},
+};
