d8ca4441f03ede354573179d5345356a76c827b6
skipprthepenguin <sagie2006p@gmail.com>
2024-04-01 16:46:47 +0300

    typo

--- data/mods/ttc_season_2_2/scripts.ts
+++ data/mods/ttc_season_2_2/scripts.ts
@@ -3,7 +3,7 @@ import { ModifyPokemon } from "../../../tools/utils/modifyPokemon";
 export const Scripts: ModdedBattleScriptsData = {
 	inherit: "ttc_season_2_1",
 	init() {
-		new ModifyPokemon('Regidraco', this)
+		new ModifyPokemon('Regidrago', this)
 			.baseStats
 				.setSPE(95);
 	},
