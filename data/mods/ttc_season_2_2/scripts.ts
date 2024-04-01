374e45cd498e034ed2146a6358fac73afb201a4d
Cody Spratford <koreanpanda345@gmail.com>
2024-03-31 18:19:11 -0700

    Update scripts.ts

--- data/mods/ttc_season_2_2/scripts.ts
+++ data/mods/ttc_season_2_2/scripts.ts
@@ -1,6 +1,10 @@
+import { ModifyPokemon } from "../../../tools/utils/modifyPokemon";
+
 export const Scripts: ModdedBattleScriptsData = {
 	inherit: "ttc_season_2_1",
 	init() {
-
+		new ModifyPokemon('Regidraco', this)
+			.baseStats
+				.setSPE(95);
 	},
 };
