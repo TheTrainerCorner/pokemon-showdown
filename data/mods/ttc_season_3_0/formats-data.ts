f0eb51273fd2b0dbf4535c8d3f9f78b624e21648
Cody Spratford <koreanpanda345@gmail.com>
2024-05-01 12:27:28 -0700

    Create formats-data.ts

---
+++ data/mods/ttc_season_3_0/formats-data.ts
@@ -0,0 +1,10 @@
+export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
+	eeveemega: {
+		isNonstandard: "Past",
+		tier: "OU",
+	},
+	eeveestarter: {
+		isNonstandard: "Past",
+		tier: "OU",
+	},
+}
