767e1d8425169d3e74ccd5d366ace435da5fe85d
skipprthepenguin <sagie2006p@gmail.com>
2024-05-05 18:32:06 +0300

    changed s2 file

--- data/mods/ttc_current/scripts.ts
+++ data/mods/ttc_current/scripts.ts
@@ -1,4 +1,4 @@
 export const Scripts: ModdedBattleScriptsData = {
-	inherit: 'ttc_season_3_0',	
+	inherit: 'ttc_season_2_0',	
 	// This folder shouldn't be touched. It is only used to direct the pointers to the correct place.
 };
