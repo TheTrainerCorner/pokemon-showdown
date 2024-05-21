fc929497241f4e436500242ec5f19bddb226ec8f
skipprthepenguin <sagie2006p@gmail.com>
2024-05-22 00:48:49 +0300

    moving back

--- data/mods/ttc_season_3_0/scripts.ts
+++ data/mods/ttc_season_3_0/scripts.ts
@@ -923,5 +923,31 @@ export const Scripts: ModdedBattleScriptsData = {
 
 		//#endregion
 
+
+		// #region Add Explosion Move Flag to moves
+		const explosionMoves: string[] = [
+			"explosion",
+			"mindblown",
+			"selfdestruct",
+			"mistyexplosion",
+			"shelltrap",
+			"eruption",
+			"eggbomb",
+			"magnetbomb",
+			"sludgebomb",
+			"seedbomb",
+			"steameruption",
+			"rockblast",
+			"mudbomb",
+			"metalburst",
+			"syrupbomb",
+			"chloroblast",
+			"cragblast",
+		];
+
+		for (const move of explosionMoves) {
+			this.modData('Moves', move).flags.explosion= 1;
+		}
+		// #endregion
 	},
 };
