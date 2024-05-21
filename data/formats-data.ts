1bd2ae1e3bb2eff553e9022e341eea002c19cced
skipprthepenguin <sagie2006p@gmail.com>
2024-05-21 11:54:38 +0300

    adding new mons

--- data/formats-data.ts
+++ data/formats-data.ts
@@ -6100,4 +6100,47 @@ export const FormatsData: {[k: string]: SpeciesFormatsData} = {
 		isNonstandard: "Custom",
 		tier: "Illegal",
 	},
+	eeveemega: {
+		isNonstandard: "Past",
+		tier: "OU",
+	},
+	meowthmega: {
+		isNonstandard: "Past",
+		tier: "OU",
+		natDexTier: "OU",
+	},
+	goopli: {
+		isNonstandard: "Past",
+		tier: "LC",
+		natDexTier: "LC",
+	},
+	slugoliath: {
+		isNonstandard: "Past",
+		tier: "OU",
+		natDexTier: "OU",
+	},
+	penquill: {
+		tier: "OU",
+		natDexTier: "OU",
+	},
+	crustocean: {
+		tier: "OU",
+		natDexTier: "OU",
+	},
+	wishiwashisoulless: {
+		tier: "OU",
+		natDexTier: "OU",
+	},
+	wishiwashiresentful: {
+		tier: "OU",
+		natDexTier: "OU",
+	},
+	empoleonmega: {
+		tier: "OU",
+		natDexTier: "OU",
+	},
+	pangoromega: {
+		tier: "OU",
+		natDexTier: "OU",
+	},
 };
