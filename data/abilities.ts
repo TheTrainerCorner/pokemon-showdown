05f5d060dbde876a12de922d03bb5dcf118e549c
skipprthepenguin <sagie2006p@gmail.com>
2024-06-20 15:48:09 +0300

    testing on normal code

--- data/abilities.ts
+++ data/abilities.ts
@@ -405,7 +405,7 @@ export const Abilities: {[abilityid: string]: AbilityData} = {
 			if (!lastAttackedBy) return;
 			const damage = move.multihit ? move.totalDamage : lastAttackedBy.damage;
 			if (target.hp <= target.maxhp / 2 && target.hp + damage > target.maxhp / 2) {
-				this.boost({spa: 1}, target, target);
+				this.boost({spa: 1, atk: 1}, target, target);
 			}
 		},
 		name: "Berserk",
