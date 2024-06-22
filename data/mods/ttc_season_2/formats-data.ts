155b208e00ae25e7c14e12a17a09e4df1c8598ad
Cody Spratford <koreanpanda345@gmail.com>
2024-06-22 13:34:31 -0700

    Chnaged Ogerpon's Tier to OU for Natdex

--- data/mods/ttc_season_2/formats-data.ts
+++ data/mods/ttc_season_2/formats-data.ts
@@ -10,5 +10,21 @@ export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
 	shedinja: {
 		tier: "OU",
 		natDexTier:"OU"
-	}
+	},
+	ogerponhearthflame: {
+		tier: "OU",
+		natDexTier: "OU",
+	},
+	ogerpon: {
+		tier: "OU",
+		natDexTier: "OU",
+	},
+	ogerponcornerstone: {
+		tier: "OU",
+		natDexTier: "OU",
+	},
+	ogerponwellspring: {
+		tier: "OU",
+		natDexTier: "OU",
+	},
 }
