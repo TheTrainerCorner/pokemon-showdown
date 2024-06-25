789ce5aeb64eec7cd9a22c99d15e7cc488f615cb
Cody Spratford <koreanpanda345@gmail.com>
2024-06-25 11:50:52 -0700

    Update scripts.ts

--- data/mods/ttc_current/scripts.ts
+++ data/mods/ttc_current/scripts.ts
@@ -1,4 +1,4 @@
 export const Scripts: ModdedBattleScriptsData = {
-	inherit: global.Config.ttcseason,	
+	inherit: global.Config.ttcseason ?? "ttc_season_3_0",	
 	// This folder shouldn't be touched. It is only used to direct the pointers to the correct place.
 };
