beae123cb1db734905bd7ab8bc6f44c7fa5db14d
skipprthepenguin <sagie2006p@gmail.com>
2024-06-25 16:19:56 +0300

    Update moves.ts

--- data/mods/ttc_season_3_0/moves.ts
+++ data/mods/ttc_season_3_0/moves.ts
@@ -541,6 +541,22 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 			desc: "If an opposing Pokemon switches out this turn, this move hits that Pokemon before it leaves the field, even if it was not the original target. If the user moves after an opponent using Flip Turn, Parting Shot, Teleport, U-turn, or Volt Switch, but not Baton Pass, it will hit that opponent before it leaves the field. Power doubles and no accuracy check is done if the user hits an opponent switching out, and the user's turn is over; if an opponent faints from this, the replacement Pokemon does not become active until the end of the turn.",
 			shortDesc: "If a foe is switching out, hits it at 2x power.",
 		},
-	}
+	},
+	fantasytongue: {
+		num: -3010,
+		name: "Fantasy Tongue",
+		accuracy: 100,
+		basePower: 95,
+		type: "Fairy",
+		pp: 10,
+		category: "Physical",
+		target: "normal",
+		priority: 0,
+		flags: {protect: 1, bite: 1, contact: 1},
+		secondary: {
+			chance: 100,
+			volatileStatus: 'taunt',
+		},
+	},
 	//#endregion
 };
