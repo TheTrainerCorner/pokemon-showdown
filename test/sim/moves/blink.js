1d990ed04677df9c4e19859fb0c4cfc0393fcf2b
Cody Spratford <koreanpanda345@gmail.com>
2024-06-28 13:14:13 -0700

    Adding in testing for Blink

---
+++ test/sim/moves/blink.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const assert = require('./../../assert');
+const common = require('./../../common');
+
+let battle;
+
+describe('Blink', () => {
+	beforeEach(() => {
+		battle = common.createBattle();
+		battle.setPlayer('p1', {team: [
+			{species: 'Jaguaptic', moves: ['blink', 'crunch']},
+		]});
+		battle.setPlayer('p2', {team: [
+			{species: 'Snorlax', moves: ['sleeptalk']},
+		]});
+	});
+	afterEach(() => {
+		battle.destroy();
+	});
+
+	it("should only put the user to sleep for one turn", () => {
+		assert.false(battle.p1.active[0].status === 'slp');
+		battle.makeChoices('move blink', 'move sleeptalk');
+		assert(battle.p1.active[0].status === 'slp');
+		let checkStats = ['atk', 'def', 'spa', 'spd', 'spe'];
+		for (const stat of checkStats) {
+			assert.statStage(battle.p1.active[0], stat, 1);
+		}
+		battle.makeChoices('move crunch', 'move sleeptalk');
+		assert(battle.p1.active[0].status === 'slp');
+		battle.makeChoices('move crunch', 'move sleep talk');
+		assert.false(battle.p1.active[0].status === 'slp');
+	});
+});
