4603aaf20ca82aea53f19e4d6021d33b1ca86c59
Cody Spratford <koreanpanda345@gmail.com>
2024-07-24 14:34:34 -0700

    added a written test for absolute zero
    Checking to see if explosion would proc the ability or not

--- test/sim/abilities/absolutezero.js
+++ test/sim/abilities/absolutezero.js
@@ -9,7 +9,7 @@ describe('Absolute Zero', () => {
 	beforeEach(() => {
 		battle = common.createBattle();
 		battle.setPlayer('p1', {team: [
-			{ species: 'Regice', ability: 'absolutezero', moves: ['ancientpower']},
+			{ species: 'Regice', ability: 'absolutezero', moves: ['ancientpower', 'explosion']},
 			{ species: 'Ninetales-Alola', ability: 'snowwarning', moves: ['sleeptalk']},
 		]});
 		battle.setPlayer('p2', {team: [
@@ -36,6 +36,12 @@ describe('Absolute Zero', () => {
 		assert.statStage(battle.p1.active[0], 'spa', 1);
 		assert(battle.p1.sideConditions.auroraveil);
 	});
+
+	it ("should not set snow if explosion is used", () => {
+		battle.makeChoices('move explosion', 'switch 2');
+		assert(battle.p2.active[0].hp === 0);
+		assert.false(battle.p1.active[0].effectiveWeather() === 'snow');
+	})
 	// it("should set weather and add auroraveil", () => {
 	// 	battle = common.createBattle([[
 	// 		{species: 'Regice', ability: 'absolutezero', moves: ['ancientpower']},
