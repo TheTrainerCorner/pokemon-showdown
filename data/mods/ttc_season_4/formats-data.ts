ec4d0912ac8af36ca1b12961e4dda9ecae47a6a8
Cody Spratford <koreanpanda345@gmail.com>
2024-10-25 18:29:30 -0700

    Adding in the grass starters

--- data/mods/ttc_season_4/formats-data.ts
+++ data/mods/ttc_season_4/formats-data.ts
@@ -11,4 +11,16 @@ export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
 		isNonstandard: "Past",
 		tier: "OU",
 	},
+	floralith: {
+		isNonstandard: "Past",
+		tier: "LC",
+	},
+	harcrown: {
+		isNonstandard: "Past",
+		tier: "NFE",
+	},
+	demetrilith: {
+		isNonstandard: "Past",
+		tier: "OU",
+	},
 };
