919504ca139a74faae36c13f831c653ab9e3700b
Cody Spratford <koreanpanda345@gmail.com>
2025-01-08 13:25:52 -0800

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -25,8 +25,7 @@ export const Formats: FormatList = [
 		name: "[Gen 9] National Dex Randoms",
 		mod: Config.ttcseason,
 		team: "random",
-		ruleset: ['PotD', 'Obtainable', 'Species Clause', 'HP Percentage Mod', 'Cancel Mod', 'Sleep Clause Mod', 'Illusion Level Mod'],
-		unbanlist: ['Mega'],
+		ruleset: ['Standard NatDex', 'Obtainable', 'Species Clause', 'HP Percentage Mod', 'Cancel Mod', 'Sleep Clause Mod', 'Illusion Level Mod'],
 	},
 	{
 		section: "TTC Season 3 Offseason",
