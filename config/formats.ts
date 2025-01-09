2bc9c79335b1d9153a629dd7b27025505481e53b
Cody Spratford <koreanpanda345@gmail.com>
2025-01-09 14:14:39 -0800

    Removing Team Preview From Randbats

--- config/formats.ts
+++ config/formats.ts
@@ -25,7 +25,7 @@ export const Formats: FormatList = [
 		name: "[Gen 9] National Dex Randoms",
 		mod: Config.ttcseason,
 		team: "random",
-		ruleset: ['Standard NatDex', 'Terastal Clause', 'Species Clause', 'Sleep Clause Mod', 'Illusion Level Mod'],
+		ruleset: ['Standard NatDex', 'Terastal Clause', 'Species Clause', 'Sleep Clause Mod', 'Illusion Level Mod', '!Team Preview'],
 	},
 	{
 		section: "TTC Season 3 Offseason",
