47a628e33850049c96030299d2db9ac4e313c5ae
Cody Spratford <koreanpanda345@gmail.com>
2025-01-13 13:20:16 -0800

    Update ladders-local.ts

--- server/ladders-local.ts
+++ server/ladders-local.ts
@@ -64,7 +64,6 @@ export class LadderStore {
 		}
 		try {
 			const data = await FS('config/ladders/' + this.formatid + '.tsv').readIfExists();
-			console.log(data);
 			const ladder: LadderRow[] = [];
 			for (const dataLine of data.split('\n').slice(1)) {
 				const line = dataLine.trim();
@@ -72,8 +71,10 @@ export class LadderStore {
 				const row = line.split('\t');
 				ladder.push([toID(row[1]), Number(row[0]), row[1], Number(row[2]), Number(row[3]), Number(row[4]), row[5]]);
 			}
-			console.log('Ladders(' + this.formatid + ') loaded tsv: ' + JSON.stringify(this.ladder));
+
 			ladderCaches.set(this.formatid, (this.ladder = ladder));
+			console.log(ladderCaches);
+			console.log('Ladders(' + this.formatid + ') loaded tsv: ' + JSON.stringify(this.ladder));
 			return this.ladder;
 		} catch {
 			console.log('Ladders(' + this.formatid + ') err loading tsv: ' + JSON.stringify(this.ladder));
