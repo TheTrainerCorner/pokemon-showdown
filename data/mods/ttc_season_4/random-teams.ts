dda9e201e3c55dedb5b40ef1b89064172970bbd6
Cody Spratford <koreanpanda345@gmail.com>
2025-01-19 17:46:37 -0800

    Revert "reformting the file"
    This reverts commit 754e6bcdc8a72d1bdc645594b160ec074cd76451.

--- data/mods/ttc_season_4/random-teams.ts
+++ data/mods/ttc_season_4/random-teams.ts
@@ -16,224 +16,7 @@ interface BattleFactorySet {
 	ivs?: Partial<StatsTable>;
 }
 
-/**
- * This is a list of abilities, items, and moves that should be limited on all teams.
- */
-const MAXES: {abilities: {[k: string]: number}, items: {[k: string]: number}, moves: {[k: string]: number}} = {
-	abilities: {
-		toxicdebris: 1, snowcloak: 1, drizzle: 1,
-	},
-	items: {
-		choicespecs: 1, choiceband: 1, choicescarf: 1, lightclay: 1,
-	},
-	moves: {
-		rapidspin: 1, batonpass: 1, stealthrock: 1, defog: 1, 
-		spikes: 1, toxicspikes: 1, ceaselessedge: 1, auroraveil: 1,
-	},
-};
-
-/**
- * This is a list of abilities, items, and moves that need something in order to work.
- * Weather abilities are placed here instead of the REQUIRED variable due to the ability not 
- * actually be required, but more needed.
- */
-const NEEDED: {abilities: {[k: string]: string}, items: {[k: string]: string}, moves: {[k: string]: string}} = {
-	abilities: {
-		// Weather Related Abilities
-		hydration: 'raindance', swiftswim: 'raindance', raindish: 'raindance',
-		waterveil: 'raindance',
-		leafguard: 'sunnyday', solarpower: 'sunnyday', chlorophyll: 'sunnyday',
-		flowergift: 'sunnyday',
-		sandforce: 'sandstorm', sandrush: 'sandstorm', sandveil: 'sandstorm',
-		snowcloak: 'snow', icebody: 'snow', slushrush: 'snow',
-
-		// Terrain Related Abilites
-		surgesurfer: "electric",
-		grasspelt: "grassy",
-	},
-	items: {
-		// Weather Related Items
-
-		// Terrain Related Items
-		electricseed: "electric",
-		psychicseed: "psychic",
-		grassyseed: "grassy",
-		mistyseed: "misty",
-	},
-	moves: {
-		// Weather Related Moves
-
-
-		// Terrain Related Moves
-		risingvoltage: "electric",
-		expandingforce: "psychic",
-		grassyglide: "grassy",
-		mistyexplosion: "misty",
-	},
-};
-
-/**
- * This is a list of abilities, items, and moves that are required to be on all teams.
- */
-const REQUIRED: {abilities: {[k: string]: string}, items: {[k: string]: string}, moves: {[k: string]: string}, families: {abilities: string[], items: string[], moves: string[]}} = {
-	abilities: {},
-	items: {},
-	moves: {
-		stealthrock: 'hazardSet', rapidspin: 'hazardClear', defog: 'hazardClear'
-	},
-	families: {
-		abilities: [],
-		items: [],
-		moves: ['hazardSet', 'hazardClear'],
-	}
-};
-/**
- * This is a list that defines what abilities, items, and moves are classified as.
- */
-const TYPES: {abilities: {[k: string]: string[]}, items: {[k: string]: string[]}, moves: {[k: string]: string[]}} = {
-	abilities: {
-		weather: [
-			'drizzle',
-			'drought', 'sundance',
-			'snowwarning', 'chillingneigh', 'asoneglastrier', 'absolutezero',
-			'sandstream', 'granitestorm'
-		],
-		terrain: [
-			'electric',
-			'grassy',
-			'psychic',
-			'misty',
-		],
-	},
-	items: {},
-	moves: {},
-};
-
-/**
- * This is a list that defines what abilities, items, and moves really are.
- */
-const SETS: {abilities: {[k: string]: {[k: string]: string}}, items: {[k: string]: {[k: string]: string}}, moves: {[k: string]: {[k: string]: string}}} = {
-	abilities: {
-		weather: {
-			drizzle: 'raindance',
-			drought: 'sunnyday', sundance: 'sunnyday',
-			snowwarning: 'snow', chillingneigh: 'snow', asoneglastrier: 'snow',
-			absolutezero: 'snow',
-			sandstream: 'sandstorm', granitestorm: 'sandstorm',
-		},
-		terrain: {
-			electricsurge: "electric",
-			psychicsurge: "psychic",
-			grassysurge: "grassy",
-			seedsower: "grassy",
-			mistysurge: "misty",
-		},
-	},
-	items: {},
-	moves: {},
-};
-
-/**
- * This is a list that defines abilities, items, and moves that cause the pokemon to become resistance.
- * NOTE: There is no reason for the moves here, but just for consistence I added it in.
- */
-const RESISTANCE: {abilities: {[k: string]: string[]}, items: {[k: string]: string[]}, moves: {[k: string]: string[]}} = {
-	abilities: {
-		colorchange: [
-			'Bug', 'Cosmic', 'Dark', 'Dragon',
-			'Electric', 'Fighting', 'Flying',
-			'Fire', 'Fairy', 'Grass', 'Ground',
-			'Ice', 'Normal', 'Poison', 'Rock',
-			'Steel', 'Water'
-		],
-		aurabreak: ['Dark', 'Psychic', 'Fairy'],
-		// Water
-		dryskin: ['Water'], waterabsorb: ['Water'], stormdrain: ['Water'],
-		silentwater: ['Water'], watercompaction: ['Water'], steamengine: ['Water', 'Fire'],
-		magmaarmor: ['Water', 'Fire'],
-		// Fire
-		flashfire: ['Fire'], heatproof: ['Fire'], mightyfire: ['Fire'],
-		// Fairy
-		sweettooth: ['Fairy'],
-		// Electric
-		lightningrod: ['Electric'], motordrive: ['Electric'], voltabsorb: ['Electric'],
-		phototaxis: ['Electric'], radiatinglight: ['Electric'],
-		// Grass
-		sapsipper: ['Grass'], naturesgift: ['Grass'],
-		// Ice
-		thickfat: ['Ice', 'Fire'],
-		// Ground
-		levitate: ['Ground'],
-		// Steel
-		garbagedisposal: ['Steel'],
-	},
-	items: {},
-	moves: {},
-};
-
-
-
 export class RandomTTCTeams extends RandomGen8Teams {
-
-	handleRejectionLaws(set: BattleFactorySet, teamData: TeamData, dex: ModdedDex): [boolean, boolean] | [boolean, boolean, number[]] {
-		const item = dex.items.get(set.item);
-		const ability = dex.abilities.get(set.ability);
-	
-		if (teamData.megaCount && teamData.megaCount > 0 && item.megaStone) {
-			return [false, false]; // move to the next set if pokemon contains a mega stone and the team already has a mega stone.
-		}
-		if (MAXES.items[item.id] && teamData.has[item.id] >=MAXES.items[item.id]) {
-			return [false, false]; // move to the next set if the pokemon contains an item that has already hit it's limit in the team.
-		}
-		if (MAXES.abilities[ability.id] && teamData.has[ability.id] >= MAXES.abilities[ability.id]) {
-			return [false, false]; // move to the next set if the pokemon contains an ability that has already hit it's limit in the team.
-		}
-		if (NEEDED.abilities[ability.id] && teamData.weather !== NEEDED.abilities[ability.id]) {
-			return [false, false]; // move to the next set if the pokemon's ability requires a weather that is not possible in the team.
-		}
-		if (teamData.weather && TYPES.abilities['weather'].includes(ability.id)) {
-			return [false, false]; // reject 2+ weather setters per team.
-		}
-		if (teamData.terrain && TYPES.abilities['terrain'].includes(ability.id)) {
-			return [false, false]; // reject 2+ terrain setters per team.
-		}
-		if (NEEDED.items[item.id] && teamData.terrain !== NEEDED.items[item.id]) {
-			return [false, false]; // move to the next set if the pokemon's item requires a terrain that is not possible in the team.
-		}
-		if (NEEDED.abilities[ability.id] && teamData.terrain !== NEEDED.abilities[ability.id]) {
-			return [false, false]; // move to the next set if the pokemon's ability requires a terrain that is not possible in the team.
-		}
-	
-		let reject = false;
-		let hasRequiredMove = false;
-		let hasRequiredAbility = false;
-		let hasRequiredItem = false;
-		const curSetVariants = [];
-		for (const move of set.moves) {
-			const variantIndex = this.random(move.length);
-			const moveId = toID(move[variantIndex]);
-			if (MAXES.moves[moveId] && teamData.has[moveId] >= MAXES.moves[moveId]) {
-				reject = true; // reject any set that has a move that exceed the max limit for the team.
-				break;
-			}
-			if (REQUIRED.moves[moveId] && !teamData.has[REQUIRED.moves[moveId]]) {
-				hasRequiredMove = true; // place into the priority pool if they have a required move.
-			}
-	
-			curSetVariants.push(variantIndex);
-		}
-	
-		if (REQUIRED.abilities[ability.id]) {
-			hasRequiredAbility = true; // place into the priority pool if they have a required ability.
-		}
-		if (REQUIRED.items[item.id]) {
-			hasRequiredItem = true;
-		}	
-		
-		return [!reject, (hasRequiredAbility || hasRequiredItem || hasRequiredMove), curSetVariants]; // Set passed the laws.
-	
-	}
-
 
 	randomFactorySets: {[format: string]: {[species: string]: BattleFactorySpecies}} = require('./factory-sets.json');
 
@@ -245,15 +28,134 @@ export class RandomTTCTeams extends RandomGen8Teams {
 		const id = toID(species.name);
 		const setList = this.randomFactorySets[tier][id].sets;
 
+		const itemsMax: {[k: string]: number} = { choicespecs: 1, choiceband: 1, choicescarf: 1, lightclay: 1, eviolite: 2};
+		const movesMax: {[k: string]: number} = {
+			rapidspin: 1,
+			batonpass: 1, 
+			stealthrock: 1, 
+			defog: 1, 
+			spikes: 1, 
+			toxicspikes: 1,
+			ceaselessedge: 1,
+			auroraveil: 1,
+		};
+		const abilitiesMax: {[k: string]: number} = {
+			toxicdebris: 1,
+			snowcloak: 1,
+			drizzle: 1,
+		};
+		let requiredAbilities: {[k: string]: string} = { drizzle: 'weather' };
+		const requiredMoves: {[k: string]: string} = {
+			stealthrock: 'hazardSet', rapidspin: 'hazardClear', defog: 'hazardClear'
+		};
+		const weatherAbilitiesRequire: {[k: string]: string} = {
+			hydration: 'raindance', swiftswim: 'raindance', raindish: 'raindance',
+			waterveil: 'raindance',
+			leafguard: 'sunnyday', solarpower: 'sunnyday', chlorophyll: 'sunnyday',
+			flowergift: 'sunnyday',
+			sandforce: 'sandstorm', sandrush: 'sandstorm', sandveil: 'sandstorm',
+			snowcloak: 'snow', icebody: 'snow', slushrush: 'snow',
+		};
+		const weatherAbilities = [
+			'drizzle',
+			'drought', 'sundance',
+			'snowwarning', 'chillingneigh', 'asoneglastrier', 'absolutezero',
+			'sandstream', 'granitestorm'
+		];
+
+		const terrainAbilities: {[k: string]: string} = {
+			electricsurge: "electric",
+			psychicsurge: "psychic",
+			grassysurge: "grassy",
+			mistysurge: "misty",
+		};
+
+		const terrainAbilitiesRequired: {[k: string]: string} = {
+			surgesurfer: "electric",
+			grasspelt: "grassy",
+		};
+
+		const terrainMovesRequired: {[k: string]: string} = {
+			risingvoltage: "electric",
+			expandingforce: "psychic",
+			grassyglide: "grassy",
+			mistyexplosion: "misty",
+		};
+
+		const terrainItemsRequire: {[k: string]: string} = {
+			electricseed: "electric",
+			psychicseed: "psychic",
+			grassyseed: "grassy",
+			mistyseed: "misty",
+		};
+
 		let effectivePool: {set: AnyObject, moveVariants?: number[], itemVariants?: number, abilityVariants?: number}[] = [];
 		const priorityPool = [];
 		for (const curSet of setList) {
 			// if (this.forceMonotype && !species.types.includes(this.forceMonotype)) continue;
-			const isLegal = this.handleRejectionLaws(curSet, teamData, this.dex)
 
-			if (isLegal[0]) continue; // move on to the next set if the set fails the checks.
-			effectivePool.push({set: curSet, moveVariants: isLegal[2]});
-			if (isLegal[1]) priorityPool.push({set: curSet, moveVariants: isLegal[2]});
+			const itemData = this.dex.items.get(curSet.item);
+			const abilityState = this.dex.abilities.get(curSet.ability);
+
+			if (teamData.megaCount && teamData.megaCount > 0 && itemData.megaStone) {
+				continue; // move to the next set if pokemon contains a mega stone and the team already has a mega stone.
+			}
+			
+			if (itemsMax[itemData.id] && teamData.has[itemData.id] >= itemsMax[itemData.id]) {
+				continue; // move to the next set if the pokemon contains an item that has already hit it's limit in the team.
+			}
+
+			if (abilitiesMax[abilityState.id] && teamData.has[abilityState.id] >= abilitiesMax[abilityState.id]) {
+				continue; // move to the next set if the pokemon contains an ability that has already hit it's limit in the team.
+			}
+
+			if (weatherAbilitiesRequire[abilityState.id] && teamData.weather !== weatherAbilitiesRequire[abilityState.id]) {
+				continue; // move to the next set if the pokemon's ability requires a weather that is not possible in the team.
+			}
+
+			if (teamData.weather && weatherAbilities.includes(abilityState.id)) {
+				continue; // reject 2+ weather setters per team.
+			}
+
+			if (teamData.terrain && terrainAbilities[abilityState.id]) {
+				continue; // reject 2+ terrain setters per team.
+			}
+
+			if (terrainItemsRequire[itemData.id] &&  teamData.terrain !== terrainItemsRequire[itemData.id]) {
+				continue; // move to the next set if the pokemon's item requires a terrain that is not possible in the team.
+			}
+
+			if (terrainAbilitiesRequired[abilityState.id] && teamData.terrain !== terrainAbilitiesRequired[abilityState.id]) {
+				continue; // move to the next set if the pokemon's ability requires a terrain that is not possible in the team.
+			}
+
+			let reject = false;
+			let hasRequiredMove = false;
+			let hasRequiredAbility = false;
+			let hasRequiredWeather = false;
+			const curSetVariants = [];
+			for (const move of curSet.moves) {
+				const variantIndex = this.random(move.length);
+				const moveId = toID(move[variantIndex]);
+				if (movesMax[moveId] && teamData.has[moveId] >= movesMax[moveId]) {
+					reject = true; // reject any set that has a move that exceed the max limit for the team.
+					break;
+				}
+				if (requiredMoves[moveId] && !teamData.has[requiredMoves[moveId]]) {
+					hasRequiredMove = true; // place into the priority pool if they have a required move.
+				}
+
+				curSetVariants.push(variantIndex);
+			}
+
+			if (requiredAbilities[abilityState.id]) {
+				hasRequiredAbility = true; // place into the priority pool if they have a required ability.
+			}
+
+
+			if (reject) continue; // move on to the next set if the set fails the checks.
+			effectivePool.push({set: curSet, moveVariants: curSetVariants});
+			if (hasRequiredMove || hasRequiredAbility) priorityPool.push({set: curSet, moveVariants: curSetVariants});
 		}
 
 		if (priorityPool.length) effectivePool = priorityPool;
@@ -306,6 +208,54 @@ export class RandomTTCTeams extends RandomGen8Teams {
 			weaknesses: {},
 			resistances: {},
 		};
+		const weatherAbilitiesSet: {[k: string]: string} = {
+			drizzle: 'raindance',
+			drought: 'sunnyday', sundance: 'sunnyday',
+			snowwarning: 'snow', chillingneigh: 'snow', asoneglastrier: 'snow',
+			absolutezero: 'snow',
+			sandstream: 'sandstorm', granitestorm: 'sandstorm',
+		};
+		const terrainAbilitiesSet: {[k: string]: string} = {
+			electricsurge: "electric",
+			psychicsurge: "psychic",
+			grassysurge: "grassy",
+			seedsower: "grassy",
+			mistysurge: "misty",
+		};
+		const resistanceAbilities: {[k: string]: string[]} = {
+			colorchange: [
+				'Bug', 'Cosmic', 'Dark', 'Dragon',
+				'Electric', 'Fighting', 'Flying',
+				'Fire', 'Fairy', 'Grass', 'Ground',
+				'Ice', 'Normal', 'Poison', 'Rock',
+				'Steel', 'Water'
+			],
+			aurabreak: ['Dark', 'Psychic', 'Fairy'],
+			// Water
+			dryskin: ['Water'], waterabsorb: ['Water'], stormdrain: ['Water'],
+			silentwater: ['Water'], watercompaction: ['Water'], steamengine: ['Water', 'Fire'],
+			magmaarmor: ['Water', 'Fire'],
+			// Fire
+			flashfire: ['Fire'], heatproof: ['Fire'], mightyfire: ['Fire'],
+			// Fairy
+			sweettooth: ['Fairy'],
+			// Electric
+			lightningrod: ['Electric'], motordrive: ['Electric'], voltabsorb: ['Electric'],
+			phototaxis: ['Electric'], radiatinglight: ['Electric'],
+			// Grass
+			sapsipper: ['Grass'], naturesgift: ['Grass'],
+			// Ice
+			thickfat: ['Ice', 'Fire'],
+			// Ground
+			levitate: ['Ground'],
+			// Steel
+			garbagedisposal: ['Steel'],
+		};
+
+		const requiredAbilityFamilies = ['weather'];
+		const requiredAbilities: {[k: string]: string} = {drizzle: 'weather'};
+		const requiredMoveFamilies = ['hazardSet', 'hazardClear'];
+		const requiredMoves: {[k: string]: string} = {stealthrock: 'hazardSet', rapidspin: 'hazardClear', defog: 'hazardClear'};
 
 		while(pokemonPool.length && pokemon.length < this.maxTeamSize) {
 			const species = this.dex.species.get(this.sampleNoReplace(pokemonPool));
@@ -361,31 +311,31 @@ export class RandomTTCTeams extends RandomGen8Teams {
 
 			const abilityState = this.dex.abilities.get(set.ability);
 
-			if (abilityState.id in SETS.abilities['weather']) {
-				teamData.weather = SETS.abilities['weather'][abilityState.id];
+			if (abilityState.id in weatherAbilitiesSet) {
+				teamData.weather = weatherAbilitiesSet[abilityState.id];
 			}
 
-			if (abilityState.id in SETS.abilities['terrain']) {
-				teamData.terrain = SETS.abilities['terrain'][abilityState.id];
+			if (abilityState.id in terrainAbilitiesSet) {
+				teamData.terrain = terrainAbilitiesSet[abilityState.id];
 			}
 
 			teamData.has[abilityState.id] = (teamData.has[abilityState.id] + 1) || 1;
 			for (const move of set.moves) {
 				const moveId = toID(move);
 				teamData.has[moveId] = (teamData.has[moveId] + 1) || 1;
-				if (moveId in REQUIRED.moves) {
-					teamData.has[REQUIRED.moves[moveId]] = 1;
+				if (moveId in requiredMoves) {
+					teamData.has[requiredMoves[moveId]] = 1;
 				}
 			}
 
-			if (REQUIRED.abilities[abilityState.id]) {
-				teamData.has[REQUIRED.abilities[abilityState.id]] = 1;
+			if (requiredAbilities[abilityState.id]) {
+				teamData.has[requiredAbilities[abilityState.id]] = 1;
 			}
 
 			for (const typeName of this.dex.types.names()) {
 				// Cover any major weakness (3+) with at least one resistance
 				if (teamData.resistances[typeName] >= 1) continue;
-				if (RESISTANCE.abilities[abilityState.id]?.includes(typeName) || !this.dex.getImmunity(typeName, types)) {
+				if (resistanceAbilities[abilityState.id]?.includes(typeName) || !this.dex.getImmunity(typeName, types)) {
 					teamData.resistances[typeName] = (teamData.resistances[typeName] || 0) + 1;
 					if (teamData.resistances[typeName] >= 1) teamData.weaknesses[typeName] = 0;
 					continue;
@@ -407,10 +357,10 @@ export class RandomTTCTeams extends RandomGen8Teams {
 		if (!teamData.forceResult) { // If the team doesn't pass these checks, then we will need to redo the team again.
 			// Double Checking that there is only one pokemon that has a mega stone per team.
 			if (teamData.megaCount && teamData.megaCount > 1) return this.randomFactoryTeam(side, ++depth);
-			for (const requiredFamily of REQUIRED.families.abilities) {
+			for (const requiredFamily of requiredAbilityFamilies) {
 				if (!teamData.has[requiredFamily]) return this.randomFactoryTeam(side, ++depth);
 			}
-			for (const requiredFamily of REQUIRED.families.moves) {
+			for (const requiredFamily of requiredMoveFamilies) {
 				if (!teamData.has[requiredFamily]) return this.randomFactoryTeam(side, ++depth);
 			}
 			for (const type in teamData.weaknesses) {
