b96c76a0a3b3642018d2ef8177e0745db1fdaa9e
Skyknight5 <skyler.j.wilson1@gmail.com>
2025-01-20 23:35:00 -0500

    Update scripts.ts

--- data/mods/ttc_learning/scripts.ts
+++ data/mods/ttc_learning/scripts.ts
@@ -1,6 +1,9 @@
+import { ModifyPokemon } from "../../../tools/utils/modifyPokemon";
+
 export const Scripts: ModdedBattleScriptsData = {
 	inherit: 'ttc_season_4',
 	init() {
-
+		new ModifyPokemon('Lopunny', this)
+		.types.setType('Cosmic','Fighting')
 	},
 };
