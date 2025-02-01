d98a1c30819458b3fa4d43598daed59b83c90157
Cody Spratford <koreanpanda345@gmail.com>
2025-02-01 15:22:38 -0800

    testing

--- data/mods/ttc_season_4/scripts.ts
+++ data/mods/ttc_season_4/scripts.ts
@@ -103,7 +103,8 @@ export const Scripts: ModdedBattleScriptsData = {
 			.pokemon.abilities
 				.setAbility1('Pressure')
 			.pokemon.baseStats
-				.setHP(75)
+				// .setHP(75)
+				.setHP(80)
 				.setATK(60)
 				.setDEF(70)
 				.setSPD(70)
