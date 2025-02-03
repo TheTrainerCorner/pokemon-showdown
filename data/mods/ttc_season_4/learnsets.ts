fb5f3ab126ee727b5e75ec67637fae8cfcd494bd
Cody Spratford <koreanpanda345@gmail.com>
2025-02-02 16:34:14 -0800

    Update learnsets.ts

--- data/mods/ttc_season_4/learnsets.ts
+++ data/mods/ttc_season_4/learnsets.ts
@@ -268,7 +268,7 @@ export const Learnsets: {[k: string]: ModdedLearnsetData} = {
 
 			// Other Moves
 			aromatherapy: ["9M"],
-			amensia: ["9M"],
+			amnesia: ["9M"],
 			batonpass: ["9M"],
 			bite: ["9M"],
 			bodyslam: ["9M"],
@@ -349,7 +349,7 @@ export const Learnsets: {[k: string]: ModdedLearnsetData} = {
 
 			// Other Moves
 			aromatherapy: ["9M"],
-			amensia: ["9M"],
+			amnesia: ["9M"],
 			batonpass: ["9M"],
 			bite: ["9M"],
 			bodyslam: ["9M"],
