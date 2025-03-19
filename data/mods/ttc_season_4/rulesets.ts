c170509ae5cb157a5fc6befa7d7c34a9070be250
Cody Spratford <koreanpanda345@gmail.com>
2025-03-19 12:26:07 -0700

    Adjusting the legal state of Snow Cloak and Sand Veil

---
+++ data/mods/ttc_season_4/rulesets.ts
@@ -0,0 +1,39 @@
+export const Rulesets: {[k: string]: ModdedFormatData} = {
+	seasoncomplexbans: {
+		inherit: true,
+		desc: "All of the Complex Bans for Season 3",
+		ruleset: [
+			'No Shell Smash Mega Toise',
+			'No Calm Mind For The Patio Set',
+			'No Dragon Dance for the Patio Set',
+		],
+	},
+	evasionclause: {
+		inherit: true,
+		// Removing the abilities that are affected by this clause
+		ruleset: ['Evasion Items Clause', 'Evasion Moves Clause']
+	},
+	seasonunbanlist: {
+		inherit: true,
+		unbanlist: [
+			// Adding this just in case
+			'Unreleased',
+			// Tiers
+			'LGPE',
+			// Abilities
+			'Snow Cloak', 'Sand Veil',
+			// Moves
+			'Barb Barrage', 'Baton Pass','Blazing Torque', 'Ceaseless Edge',
+			'Chloroblast', 'Combat Torque', 'Dire Claw',
+			"Dragon's Maw", 'Esper Wing', 'Infernal Parade',
+			'Magical Torque', 'Mountain Gale', 'Noxious Torque',
+			'Psyshield Bash', 'Raging Fury', 'Shelter',
+			'Springtide Storm', 'Stone Axe', 'Triple Arrows',
+			'Victory Dance', 'Wicked Torque',
+			// Items
+			'Custap Berry', 'Thick Club', 'Full Incense',
+			'Lax Incense', 'Odd Incense', 'Rock Incense',
+			'Rose Incense', 'Sea Incense', 'Wave Incense',
+		],
+	}
+};
