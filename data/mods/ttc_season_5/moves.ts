9ee38647fb43a2b8221d145dca62eaefbb37a780
Cody Spratford <koreanpanda345@gmail.com>
2025-08-24 23:09:45 -0700

    Added Changes in from Abomasnow to Ampharos-Mega
    Pixie Burst was added!

--- data/mods/ttc_season_5/moves.ts
+++ data/mods/ttc_season_5/moves.ts
@@ -1,3 +1,21 @@
 export const Moves: {[k: string]: ModdedMoveData} = {
-
+	pixieburst: {
+		accuracy: 100,
+		basePower: 95,
+		category: "Special",
+		name: "Pixie Burst",
+		pp: 24,
+		priority: 0,
+		flags: { protect: 1, mirror: 1 },
+		secondary: {
+			chance: 20,
+			boosts: {
+				spa: -1,
+			},
+		},
+		target: "normal",
+		type: "Fairy",
+		desc: "Has a 20% chance to lower the target's Special Attack by 1 stage.",
+		shortDesc: "20% chance to lower the target's Sp. Atk by 1.",
+	},
 };
--- data/mods/ttc_season_5/scripts.ts
+++ data/mods/ttc_season_5/scripts.ts
@@ -5,5 +5,112 @@ export const Scripts: ModdedBattleScriptsData = {
 	inherit: 'ttc_season_4',
 	init() {
 
+		// Pokemon Changes
+		new ModifyPokemon('Abomasnow', this)
+			.baseStats
+				.setDEF(85)
+			.pokemon.learnset
+				.add('icehammer');
+		new ModifyPokemon('Abomasnow-Mega', this)
+			.baseStats
+				.setDEF(110)
+				.setSPD(115);
+		new ModifyPokemon('Absol', this)
+			.abilities
+				.setAbility0('Magic Bounce')
+			.pokemon.learnset
+				.add('Doom Desire');
+		new ModifyPokemon('Absol-Mega', this)
+			.abilities
+				.setAbility0('Suresight');
+		new ModifyPokemon('Accelgor', this)
+			.abilities
+				.setAbility0('Dry Skin')
+				.setAbility1('Stakeout')
+			.pokemon.learnset
+				.add('Quick Guard')
+				.add('Counter')
+				.add('Strange Steam')
+				.add('Razor Wind')
+				.remove('Signal Beam');
+		new ModifyPokemon('Acudraco', this);
+		new ModifyPokemon('Aerodactyl', this)
+			.baseStats
+				.setHP(90)
+				.setATK(120)
+				.setDEF(73)
+				.setSPE(132)
+			.pokemon.learnset
+				.add('Metal Claw')
+				.add('Mighty Cleave')
+				.remove('Rock Slide');
+		new ModifyPokemon('Aerodactyl-Mega', this)
+			.baseStats
+				.setHP(90)
+				.setATK(140)
+				.setDEF(93)
+				.setSPE(152);
+		new ModifyPokemon('Aggron', this)
+			.types
+				.setType('Steel')
+			.pokemon.learnset
+				.add('Steel Spikes')
+				.remove('Cut');
+		new ModifyPokemon('Alakazam', this)
+			.baseStats
+				.setHP(75)
+				.setATK(49)
+				.setSPE(121);
+		new ModifyPokemon('Alakazam-Mega', this)
+			.abilities
+				.setAbility0("Mind's Eye")
+			.pokemon.baseStats
+				.setHP(75)
+				.setATK(59)
+				.setSPE(136);
+		new ModifyPokemon('Alcremie', this)
+			.learnset
+				.add('Pixie Burst')
+				.remove('Moonblast');
+		new ModifyPokemon('Alomomola', this)
+			.baseStats
+				.setSPA(50)
+			.pokemon.learnset
+				.add('Charm');
+		new ModifyPokemon('Altaria', this)
+			.baseStats
+				.setATK(65)
+				.setDEF(75)
+				.setSPA(105)
+				.setSPE(70)
+			.pokemon.learnset
+				.add('Pixie Burst')
+				.remove('Moonblast');
+		new ModifyPokemon('Ambipom', this)
+			.learnset
+				.add('Crag Blast')
+				.add('Rock Blast')
+				.add('Follow Me');
+		new ModifyPokemon('Amoonguss', this)
+			.baseStats
+				.setHP(108)
+				.setATK(89)
+				.setDEF(89)
+				.setSPA(89)
+				.setSPD(94)
+			.pokemon.learnset
+				.add('Teeter Dance')
+				.add('Chloroblast');
+		new ModifyPokemon('Ampharos', this)
+			.abilities
+				.setHiddenAbility('Illuminate')
+			.pokemon.baseStats
+				.setATK(80)
+				.setDEF(90);
+		new ModifyPokemon('Ampharos-Mega', this)
+			.baseStats
+				.setDEF(110)
+				.setSPE(50);
+		
 	},
  };
