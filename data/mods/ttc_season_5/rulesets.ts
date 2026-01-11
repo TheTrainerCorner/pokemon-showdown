6ff582ea463d5dd81594bdb72a83bf0c7678ba76
Cody Spratford <36426888+koreanpanda345@users.noreply.github.com>
2026-01-11 13:54:17 -0800

    Adjusted the Mega Patio set's complex ban to allow DDance and Calm Mind

---
+++ data/mods/ttc_season_5/rulesets.ts
@@ -0,0 +1,15 @@
+import { ModdedFormatData } from "../../../sim/dex-formats";
+
+export const Rulesets: {[k: string]: ModdedFormatData} = {
+	seasoncomplexbans: {
+	inherit: true,
+		ruleset: [
+			'No Shell Smash Mega Toise',
+			'No Dragon Dance For Kyurem-Black',
+			'No Last Respects For Houndstone',
+			'No Last Respects For Basculegion',
+			'No Power Construct For Zygarde',
+			'No Power Construct For Zygarde-10%',
+		],
+	},
+};
