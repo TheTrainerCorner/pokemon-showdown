b6718ea7cc680b2bb55486ef9f38af473f66f492
Cody Spratford <36426888+koreanpanda345@users.noreply.github.com>
2026-01-11 15:20:58 -0800

    Adjusted the ruleset again

--- data/mods/ttc_season_5/rulesets.ts
+++ data/mods/ttc_season_5/rulesets.ts
@@ -3,13 +3,9 @@ import { ModdedFormatData } from "../../../sim/dex-formats";
 export const Rulesets: {[k: string]: ModdedFormatData} = {
 	seasoncomplexbans: {
 	inherit: true,
+		desc: "All of the Complex Bans for Season 4",
 		ruleset: [
 			'No Shell Smash Mega Toise',
-			'No Dragon Dance For Kyurem-Black',
-			'No Last Respects For Houndstone',
-			'No Last Respects For Basculegion',
-			'No Power Construct For Zygarde',
-			'No Power Construct For Zygarde-10%',
 		],
 	},
 };
